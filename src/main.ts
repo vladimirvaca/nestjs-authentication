80a91c6056972d13db85cc52ec31ab0e190da938
Vladimir Vaca <ramvlay@gmail.com>
2024-08-09 23:55:16 -0500

    Added open-api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Basic authentication - Nestjs')
+    .setDescription('Example of a basic authentication using Nestjs and Jwt')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
